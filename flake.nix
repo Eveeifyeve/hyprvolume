f4f61dc99ea05982ead61c81cc43e3af8560baad
eveeifyeve <88671402+Eveeifyeve@users.noreply.github.com>
2024-11-01 03:40:03 +1100

    feat: precommit init

--- flake.nix
+++ flake.nix
@@ -3,6 +3,11 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-parts.url = "github:hercules-ci/flake-parts";
     fenix.url = "github:nix-community/fenix";
+
+		pre-commit-hooks = {
+      url = "github:cachix/git-hooks.nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs = inputs@{ nixpkgs, flake-parts, ... }:
@@ -23,6 +28,18 @@
           ];
         };
 
+				checks = {
+					pre-commit-check = inputs.pre-commit-hooks.lib.${system}.run {
+            src = ./.;
+            hooks = {
+							rustfmt.enable = true;
+							cargo-check.enable = true;
+							clippy.enable = true;
+              nixfmt-rfc-style.enable = true;
+            };
+          };
+				};
+
         devShells.default = with pkgs; let 
         toolchain = pkgs.fenix.stable.withComponents [
           "rustc"
@@ -31,12 +48,29 @@
         ];
         in mkShell
         {
+					inherit (self.checks.${system}.pre-commit-check) shellHook;
+					buildInputs = self.checks.${system}.pre-commit-check.enabledPackages;
           packages = with pkgs; [
             openssl
             rust-analyzer
             toolchain
           ];
         };
+
+				# Feel free to make a nixpkg for it long as you maintain it. 
+				packages.default = pkgs.rustPlatform.buildRustPackage rec {
+					pname = "hyprvolume";
+					version = "1.0.0";
+					
+					src = pkgs.fetchFromGithub {
+						owner = "eveeifyeve";
+						repo = "hyprvolume";
+						rev = "v${version}";
+						hash = lib.fakeHash;
+					};
+					cargoHash = lib.fakeHash;
+				};
+
       };
     };
 }
